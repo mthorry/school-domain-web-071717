33bcee3ea81f38ead5f9b1080c5700c0e3353f49
mthorry <mthorry@gmail.com>
2017-07-19 11:23:18 -0400

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require "pry"
+
+class School
+
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil #if no grade given exists
+      @roster[grade] = []    #create an array within roster hash for grade
+    end
+      @roster[grade] << name #add students to that array
+  end
+
+
+  def grade(num) #DO not want to use grade as arg here
+    @roster[num] #enter a NUMBER and get back that array of students
+  end
+
+  def sort
+    @roster.each do |num, students| #iterates through the entire roster
+      students.sort! #for each array of students, alphabetizes ALL! students
+    end
+  end
+
+
+end
